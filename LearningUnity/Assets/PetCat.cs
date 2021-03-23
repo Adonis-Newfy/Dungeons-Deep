c0eb9b3ae29ed5de65e1c2de2ee1a2a1bb4b6e38
Nicholas <nmcanstey@mun.ca>
2021-03-23 16:12:23 -0230

    Version 0.1.1
    Added the HUBCat to the HUB World. My b.

---
+++ LearningUnity/Assets/PetCat.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetCat : MonoBehaviour
+{
+    public GameObject catMenu;
+
+    public GameObject panelA;
+    public GameObject panelB;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            catMenu.SetActive(true);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        catMenu.SetActive(false);
+        panelA.SetActive(true);
+        panelB.SetActive(false);
+    }
+}
